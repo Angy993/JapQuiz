94b25ebc4310362a6ba4a64c7f35eba7b95270b5
Angy993 <ciccia.angelo@icloud.com>
2025-04-20 12:37:35 +0200

    Add files via upload

---
+++ new_romaji_quiz_data.js
@@ -0,0 +1,46 @@
+const data = [
+  {
+    "italiano": "Mangiare",
+    "romaji": "taberu",
+    "frase": "Watashi wa sushi o taberu.",
+    "traduzione": "Mangio sushi.",
+    "spiegazioni": {
+      "wa": {
+        "descrizione": "Particella del tema.",
+        "uso": "Indica l’argomento principale della frase.",
+        "esempio": "Watashi wa gakusei desu.",
+        "nota": "Si scrive は ma si legge 'wa'.",
+        "perche": "Perché \"Watashi\" è il tema della frase."
+      },
+      "o": {
+        "descrizione": "Particella dell’oggetto diretto.",
+        "uso": "Indica ciò che riceve l’azione.",
+        "esempio": "Ringo o taberu.",
+        "nota": "Si scrive を ma si legge 'o'.",
+        "perche": "Perché \"sushi\" è l'oggetto diretto della frase."
+      }
+    }
+  },
+  {
+    "italiano": "Bere",
+    "romaji": "nomu",
+    "frase": "Watashi wa mizu o nomu.",
+    "traduzione": "Bevo acqua.",
+    "spiegazioni": {
+      "wa": {
+        "descrizione": "Particella del tema.",
+        "uso": "Indica l’argomento principale della frase.",
+        "esempio": "Watashi wa gakusei desu.",
+        "nota": "Si scrive は ma si legge 'wa'.",
+        "perche": "Perché \"Watashi\" è il tema della frase."
+      },
+      "o": {
+        "descrizione": "Particella dell’oggetto diretto.",
+        "uso": "Indica ciò che riceve l’azione.",
+        "esempio": "Ringo o taberu.",
+        "nota": "Si scrive を ma si legge 'o'.",
+        "perche": "Perché \"mizu\" è l'oggetto diretto della frase."
+      }
+    }
+  }
+];
